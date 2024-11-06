c1d41321fa86cf65cea807ccf9c647d85777f780
ThuanNM <thuannm@vmogroup.com>
2024-11-07 03:04:48 +0700

    refactor: code

--- src/middlewares/verifyToken.middleware.ts
+++ src/middlewares/verifyToken.middleware.ts
@@ -4,6 +4,7 @@ import { TokenTypes } from '../common/constants/token.constant';
 import { RoleTypes } from '../common/constants/role.constant';
 import { ApiResponse } from '../types/ApiResponse';
 import { CustomRequest } from '../types/express';
+import { responseInterceptor } from '../common/interceptors/responseError.interceptor';
 
 export const verifyTokenUser = async (req: CustomRequest, res: Response, next: NextFunction) => {
   const tokenBearer = req.header('Authorization');
@@ -19,29 +20,17 @@ export const verifyTokenUser = async (req: CustomRequest, res: Response, next: N
   try {
     const verified = await tokenService.verifyToken(token, TokenTypes.ACCESS);
     if (!verified) {
-      const response: ApiResponse = {
-        success: false,
-        message: 'Unauthorized',
-      }
-      return res.status(401).json(response);
+      return responseInterceptor(res, 401, 'Unauthorized', null);
     }
 
     if (verified.role != RoleTypes.USER) {
-      const response: ApiResponse = {
-        success: false,
-        message: 'Không có quyền truy cập',
-      }
-      return res.status(401).json(response);
+      return responseInterceptor(res, 401, 'No permission', null);
     }
 
     req.user = verified;
     next();
   } catch (err) {
-    const response: ApiResponse = {
-      success: false,
-      message: 'Có lỗi xảy ra',
-    }
-    res.status(400).json(response);
+    responseInterceptor(res, 500, 'Có lỗi xảy ra', null);
   }
 }
 
@@ -49,11 +38,7 @@ export const verifyTokenAdmin = async (req: CustomRequest, res: Response, next: 
   const tokenBearer = req.header('Authorization');
   const token = tokenBearer?.split('Bearer')[1].trim();
   if (!token) {
-    const response: ApiResponse = {
-      success: false,
-      message: 'Unauthorized',
-    }
-    return res.status(401).json(response);
+    return responseInterceptor(res, 401, 'Unauthorized', null);
   }
 
   try {
@@ -67,21 +52,12 @@ export const verifyTokenAdmin = async (req: CustomRequest, res: Response, next: 
     }
 
     if (verified.role != RoleTypes.ADMIN) {
-      const response: ApiResponse = {
-        success: false,
-        message: 'Không có quyền truy cập',
-      }
-      return res.status(401).json(response);
+      return responseInterceptor(res, 401, 'No permission', null);
     }
 
     req.user = verified;
     next();
   } catch (err) {
-    console.log(err);
-    const response: ApiResponse = {
-      success: false,
-      message: 'Có lỗi xảy ra',
-    }
-    res.status(400).json(response);
+    responseInterceptor(res, 500, 'Có lỗi xảy ra', null);
   }
 }
